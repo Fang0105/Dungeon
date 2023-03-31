11c9fe81c04b05366728490915e72d8ea6bf7e73
Fang0105 <nody0105@gmail.com>
2023-03-31 16:05:35 +0800

    finish weapon.h

--- weapon.h
+++ weapon.h
@@ -1,10 +1,28 @@
 #ifndef __WEAPON_H
 #define __WEAPON_H
 
+#include<iostream>
+
 using namespace std;
+
 class Weapon{
+    private:
+        string name;
+        int level;
+        string occupation;
+        int durability;        
     public:
-        int haha;
+        Weapon();
+        Weapon(string, int, string, int);
+        void consume();
+        void setName(string);
+        void setLevel(string);
+        void setOccupation(string);
+        void setDurability(int);
+        string getName();
+        int getLevel();
+        string getOccupation();
+        int getDurability();
 };
 
 #endif
